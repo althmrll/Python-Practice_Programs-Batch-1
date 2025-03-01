1d60dd0917d66959764e13b07cc6ab0df693e152
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-01 21:54:10 +0800

    Fixing code 1 (success)

--- Problem 4.py
+++ Problem 4.py
@@ -1,12 +1,12 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
-whiel True:
-one= int(input("Enter 1st number:"))
-two= int(input("Enter 2nd numbner:"))
+while True:
 
-try:
-    product = one * two
-    print ("The product of", one, "and", two, "is", product,".")
-    
-except ValueError:
-    print ("!!INVALID INPUT. PLEASE ENTER A NUMBER")
+    try:
+        one= int(input("Enter 1st number:"))
+        two= int(input("Enter 2nd number:"))
+        product = one * two
+        print ("The product of", one, "and", two, "is", product,".")
+
+    except ValueError:
+        print ("!!INVALID INPUT. PLEASE ENTER A NUMBER")
