c7e4d48fa895db08ab1d3317fdc012cf1c537a24
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-01 12:29:10 +0800

    Code Fixing Start

--- Problem 1.py
+++ Problem 1.py
@@ -1,16 +1,17 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 while True:
-    one= int(input("Enter 1st numebr:"))
-    two= int(input("Enter 2nd number:"))
-
     try:
+        one= int(input("Enter 1st number:"))
+        wo= int(input("Enter 2nd number:"))
+
         if one>two:
             print (one, "is bigger.")
         elif one<two:
             print(two, "is bigger.")
         else:
             print (one, "and", two, "are equal.")
+            
     except ValueError:
         print ("!!INVALID INPUT. PLEASE TYPE A NUMBER!!")
 
