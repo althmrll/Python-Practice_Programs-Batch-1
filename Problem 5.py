a1a9c627b463e632db8f74d2b107cf56f6d817e6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-01 22:13:10 +0800

    Initial Coding

---
+++ Problem 5.py
@@ -0,0 +1,10 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+while True:
+    try:
+        one= int(input("Enter 1st number:"))
+        two= int(input("Enter 2nd number:"))
+        quotient= one/two
+        print ("the quotient of", one, "and", two, "is", quotient, ".")
+    except ValueError:
+        print ("!!INVALID INPUT. PLEASE ENTER A NUMBER!!")
