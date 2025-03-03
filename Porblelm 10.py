181b4bbe6cbc3341b896278eff39824b841a766f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-03-03 15:16:19 +0800

    Code fixing end (final code)

--- Porblelm 10.py
+++ Porblelm 10.py
@@ -1,5 +1,5 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
-for i in range (1,101):
-    if i!=i/10:
+for i in range (0,101):
+    if i%10!=0:
         print (i)
